4ae20125a033a8d667ade73fb9af22fabd46f95a
TR-194 <roesslertimon@web.de>
2024-11-26 06:44:16 +0100

    fixed Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/products', pathMatch: 'full'},
-    { path: 'products', loadChildren: () => import('./products/component/products.component').then(m => m.ProductsComponent) },
-    { path: 'cart', loadChildren: () => import('./cart/component/cart.component').then(m => m.CartComponent) },
-    { path: 'product/:id', loadChildren: () => import('./products/product-detail/product-detail.module').then(m => m.ProductDetailModule) },
+    { path: 'products', loadComponent: () => import('./products/component/products.component').then(m => m.ProductsComponent) },
+    { path: 'cart', loadComponent: () => import('./cart/component/cart.component').then(m => m.CartComponent) },
+    { path: 'product/:id', loadComponent: () => import('./products/product-detail/product-detail.module').then(m => m.ProductDetailModule) },
     { path: '**', loadComponent: () => import('./route-not-found/route-not-found.component').then(m => m.RouteNotFoundComponent) },
 ]
 
